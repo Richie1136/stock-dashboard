a97abfc0a09784547ecec383a19bffde8311201a
Richie Hagenah <richiehagenah@Mac.home>
2026-07-03 12:15:19 -0400

    Removed non us stocks from sugguestions and made the sugguestions clickable, as well as added a clear button

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -10,45 +10,64 @@ const Header = ({ setSymbol }) => {
         if (!searchStock?.trim()) return
         setSymbol(searchStock?.trim().toUpperCase())
         setSearchStock("")
+        setSugguestions([])
     }
 
+    const handleClear = (e) => {
+        e.stopPropagation()
+        setSearchStock("")
+        setSugguestions([])
+    }
     console.log(searchStock)
+
+    const handleSugguestionClick = (symbol) => {
+        setSymbol(symbol)
+        setSearchStock("")
+        setSugguestions([])
+    }
 
 
     useEffect(() => {
-        if (!searchStock) return
-        try {
-            const getTypedStock = async () => {
-                const response = await fetch(`http://localhost:5001/api/search?query=${searchStock}`)
-                console.log(response)
-                const data = await response.json()
-                console.log(data)
-                setSugguestions(data.result)
-            }
-            getTypedStock()
-        } catch (error) {
-            console.log(error)
-            console.error("Error fetching company data:", error)
+        if (!searchStock.trim) {
+            setSugguestions([])
+            return
         }
+        const getTypedStock = async () => {
+            try {
+                const response = await fetch(`http://localhost:5001/api/search?query=${searchStock}`)
+                const data = await response.json()
+                const useOnlyStock = data?.result?.filter((stock) => {
+                    return stock.type === "Common Stock" && !stock.displaySymbol.includes(".")
+                })
+                setSugguestions(useOnlyStock)
+            } catch (error) {
+                console.log(error)
+                console.error("Error fetching company data:", error)
+            }
+        }
+        getTypedStock()
     }, [searchStock])
-
-    console.log(sugguestions)
-
 
     return (
         <div className="header">
             <h1>Stock Dashboard</h1>
             <div className='search-bar-container'>
-                <input value={searchStock} placeholder="Search for Stock" onChange={(e) => setSearchStock(e.target.value)}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') handleSearch()
-                    }} />
+                <div className="search-input-container">
+                    <input value={searchStock} placeholder="Search for Stock" onChange={(e) => setSearchStock(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') handleSearch()
+                        }}
+                    />
+                    <button className="clear-button" onClick={handleClear}>x</button>
+                </div>
                 <button disabled={!searchStock?.trim()} className="search-button" onClick={handleSearch}>Search</button>
                 <div className="sugguestions">
-                    {sugguestions?.map((stock) => {
+                    {sugguestions?.map((stock, index) => {
                         const { description, symbol } = stock
                         return (
-                            <div className="suggestion-item" key={symbol}>
+                            <div className="suggestion-item" style={{ cursor: 'pointer' }} key={`${symbol}-${index}`}
+                                onClick={() => handleSugguestionClick(symbol)}
+                            >
                                 {description} ({symbol})
                             </div>
                         )
